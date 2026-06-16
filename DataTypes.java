97a34ea00b4e118bfbec46072aaf35d2f50e86a1
humayranuha <nuhahumayra24@gmail.com>
2026-06-16 13:36:26 +0600

    feat: java data types

---
+++ DataTypes.java
@@ -0,0 +1,25 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // Number data types
+        int myInt = 100;
+        float myFloat = 3.14f;
+        double myDouble = 3.14159d;
+
+        // Boolean data type
+        boolean isJavaFun = true;
+
+        // Character data type
+        char myChar = 'a';
+
+        // String data type 
+        String myString = "Hello, Java!";
+
+        // Output the values
+        System.out.println("Integer: " + myInt);
+        System.out.println("Float: " + myFloat);    
+        System.out.println("Double: " + myDouble);
+        System.out.println("Boolean: " + isJavaFun);
+        System.out.println("Character: " + myChar);
+        System.out.println("String: " + myString);
+    }
+}
