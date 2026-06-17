70b8e4241ce7b96fa5638e1e5d0a7cf27a760b9b
humayranuha <nuhahumayra24@gmail.com>
2026-06-17 18:21:21 +0600

    feat: encapsulation class

---
+++ Encapsulation.java
@@ -0,0 +1,13 @@
+public class Encapsulation {
+    private String name;
+
+    // Getter method
+    public String getName() {
+        return name;
+    }
+
+    // Setter method
+    public void setName(String name) {
+        this.name = name;
+    }
+}
