a4f5ff6cce2475ede741edee907a8ce239ccb795
humayranuha <nuhahumayra24@gmail.com>
2026-06-17 18:23:56 +0600

    feat: encapsulation implemented with get and set method

---
+++ EncapsulationMain.java
@@ -0,0 +1,7 @@
+public class EncapsulationMain {
+    public static void main(String[] args) {
+        Encapsulation person = new Encapsulation();
+        person.setName("Alice");
+        System.out.println(person.getName());
+    }
+}
