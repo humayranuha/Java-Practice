27a7cf6eb46ee4764893ba232d6183d4f4631e24
humayranuha <nuhahumayra24@gmail.com>
2026-06-17 14:05:46 +0600

    refactor: static removed

--- Car.java
+++ Car.java
@@ -1,9 +1,9 @@
 public class Car {
-    static void fullThrottle() {
+    public void fullThrottle() {
         System.out.println("The car is going as fast as it can!");
     }
 
-    static void speed(int maxSpeed) {
+    public void speed(int maxSpeed) {
         System.out.println("Max speed is: " + maxSpeed);
     }
 }
